d6069b8acb88b1dda49958f0aabfee0eb002b71e
ChanStormstout <1074357006@qq.com>
2023-11-06 01:33:39 +0800

    Move a test-case to the specific dir.

--- wrappers/knowledge_receiver.py
+++ wrappers/knowledge_receiver.py
@@ -1,4 +1,5 @@
 import re
+import extract_info
 from KnowledgeReceiverBase import KnowledgeBase
 
 class KnowledgeReceiver(KnowledgeBase):
@@ -14,13 +15,6 @@ class KnowledgeReceiver(KnowledgeBase):
     @extract_knowledge.setter
     def extract_knowledge(self, func_info):
         self._extract_knowledge_func = func_info
-    
-    # todo: _create_summary should use SK framework to return the summary of target function.
-    def func_summarizer(self, text):
-        summary = self._create_summary(text)
-        return summary
-    def _create_summary(self, text):
-        return text[:100]
 
     def receive_document(self):
         self.document_content = input("请输入文档内容: ")
@@ -30,3 +24,14 @@ class KnowledgeReceiver(KnowledgeBase):
     
     def receive_use_case(self):
         self.use_case = input("请输入使用案例: ")
+    
+    # todo: _create_summary should use SK framework to return the summary of target function.
+    def func_summarizer(self):
+        summary = self._create_summary(self.source_code)
+        return summary
+    def _create_summary(self, text):
+        return text[:100]
+    
+    # TODO: extract the information of dependencies
+    def get_dependencies(self):
+        self.dependencies = extract_info.extract_dependencies_from_file()
