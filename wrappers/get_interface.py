2e5dc8eb74447d0cc2a152c68710f18ea7c932b4
ChanStormstout <1074357006@qq.com>
2023-11-08 01:33:39 +0800

    [Dev] Delete some useless files.

--- wrappers/get_interface.py
+++
@@ -1,8 +0,0 @@
-import re
-from knowledge_receiver import KnowledgeReceiver
-# from c_fuzzer_wrapper import 
-def get_fuzzable_output():
-    # code to get the fuzzable output
-    pass
-
-output = get_fuzzable_output()
